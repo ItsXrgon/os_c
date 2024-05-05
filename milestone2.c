a98ff296613d0352e0a7550f75066aaea721f2de
itsXrgon <ali.koheil@gmail.com>
2024-05-05 23:04:57 +0300

    init

---
+++ milestone2.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+
+
+#define MEMORY_SIZE 60
+#define PROCESS_VARS_SIZE 3
+
+struct pcb {
+    int pid;
+    int priority;
+    char *state;
+    int program_counter;
+    int memory_boundaries[2];
+};
+
+struct memory_data {
+    int memory_words[MEMORY_SIZE];
+    int variables[PROCESS_VARS_SIZE];
+    struct pcb process_table[3];
+};
+
+struct scheduler { 
+    struct pcb queue1[3];
+    struct pcb queue2[3];
+    struct pcb queue3[3];
+    struct pcb queue4[3];
+    struct pcb blocked_queue[3];
+};
+
+void allocate_memory(struct pcb *process, int start_address) {
+    process->memory_boundaries[0] = start_address;
+    process->memory_boundaries[1] = start_address + PROCESS_VARS_SIZE; 
+}
+
+void free_memory(struct pcb *process) {
+    process->memory_boundaries[0] = -1;
+    process->memory_boundaries[1] = -1;
+}
+
+void print(char *str)
+{
+    printf("%s\n", str);
+}
+
+void assign(char *variable, int *variables)
+{
+    int value;
+    printf("Enter value: ");
+    scanf("%d", &value);
+
+    variables[variable[0] - 'A'] = value;
+}
+
+void write_file(char *filename, char *content)
+{
+    FILE *file = fopen(filename, "w");
+    if (file == NULL)
+    {
+        print("Error opening file");
+        return;
+    }
+    fprintf(file, "%s", content);
+    fclose(file);
+}
+
+void read_file(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        print("Error opening file");
+        return;
+    }
+    char c;
+    while ((c = fgetc(file)) != EOF)
+    {
+        printf("%c", c);
+    }
+    fclose(file);
+}
+
+void print_from_to(int from, int to)
+{
+    for (int i = from; i <= to; i++)
+    {
+        printf("%d\n", i);
+    }
+}
+
+void sem_wait(
+    int *semaphore,
+    int *mutex,
+    int *waitCount
+)
+{
+    *waitCount = *waitCount + 1;
+    if (*waitCount <= 0)
+    {
+        *mutex = 0;
+        while (*waitCount <= 0)
+        {
+            // Do nothing
+        }
+        *mutex = 1;
+    }
+}
+
+void sem_signal(
+    int *semaphore,
+    int *mutex,
+    int *waitCount
+)
+{
+    *waitCount = *waitCount - 1;
+    if (*waitCount <= 0)
+    {
+        *mutex = 1;
+    }
+}
+
+void create_process(struct pcb *process, int pid, int priority, char *state, int program_counter)
+{
+    process->pid = pid;
+    process->priority = priority;
+    process->state = state;
+    process->program_counter = program_counter;
+}
+
+void destroy_process(struct pcb *process)
+{
+    process->pid = -1;
+    process->priority = -1;
+    process->state = "DEAD";
+    process->program_counter = -1;
+}
+
+void add_to_queue(struct pcb *process, struct pcb *queue)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (queue[i].pid == -1)
+        {
+            queue[i] = *process;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    struct memory_data memory;
+
+    printf("Hello world");
+    return 0;
+}
