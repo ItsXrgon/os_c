3def6b3f1fe4eb88f91ec2ce9417ef6af00e60ff
Nasseryoyo <youussefnasser@gmail.com>
2024-05-20 14:14:50 +0300

    mutex implementation

--- milestone2.c
+++ milestone2.c
@@ -19,20 +19,18 @@ typedef struct {
     char program_file[20];
     int variables[3];
     int arrival_time;
-} Process;
+    } Process;
 
-typedef struct{
+typedef struct {
     int semaphore;
-} mutex;
+    } mutex;
 
 mutex userInputMutex;
 mutex userOutputMutex;
 mutex fileMutex;
 
-int memory[MEMORY_SIZE] = {0};
-int userInput = 1;
-int userOutput = 1;
-int file = 1;
+int memory[MEMORY_SIZE] = { 0 };
+
 int current_time = 0;
 int time_quantum;
 
@@ -44,103 +42,111 @@ void write_file(const char* filename, const char* content) {
     if (file == NULL) {
         printf("Error opening file\n");
         return;
-    }
+        }
     fprintf(file, "%s", content);
     fclose(file);
-}
+    }
 
 void read_file(const char* filename, char* buffer, size_t size) {
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file\n");
         return;
-    }
+        }
     size_t len = fread(buffer, 1, size, file);
     buffer[len] = '\0';
     fclose(file);
-}
+    }
 
 void print_from_to(int from, int to) {
     for (int i = from; i <= to; i++) {
         printf("%d\n", i);
+        }
     }
-}
 
 void assign(char* variable, char* value) {
     printf("Enter value for %s: ", variable);
     scanf("%s", value);
-}
+    }
 
 void print(char* str) {
     printf("%s\n", str);
-}
+    }
 
 void sem_wait(int* semaphore) {
     while (*semaphore <= 0) {
         // Busy wait
-    }
+        }
     (*semaphore)--;
-}
+    }
 
 void sem_signal(int* semaphore) {
     (*semaphore)++;
-}
+    }
 
 void print_memory() {
     printf("Memory: ");
     for (int i = 0; i < MEMORY_SIZE; i++) {
         printf("%d ", memory[i]);
-    }
+        }
     printf("\n");
-}
+    }
 
 void execute_instruction(Process* process, char* instruction) {
     char command[20], arg1[20], arg2[20];
-    static char file_name[100] = {0}, file_data[100] = {0}, buffer[100] = {0};
+    static char file_name[100] = { 0 }, file_data[100] = { 0 }, buffer[100] = { 0 };
 
     if (sscanf(instruction, "%s %s %s", command, arg1, arg2) < 1) {
         return;
-    }
+        }
 
     printf("Executing instruction: %s\n", instruction);
 
     if (strcmp(command, "semWait") == 0) {
-        if (strcmp(arg1, "userInput") == 0) sem_wait(&userInput);
-        if (strcmp(arg1, "userOutput") == 0) sem_wait(&userOutput);
-        if (strcmp(arg1, "file") == 0) sem_wait(&file);
-    } else if (strcmp(command, "semSignal") == 0) {
-        if (strcmp(arg1, "userInput") == 0) sem_signal(&userInput);
-        if (strcmp(arg1, "userOutput") == 0) sem_signal(&userOutput);
-        if (strcmp(arg1, "file") == 0) sem_signal(&file);
-    } else if (strcmp(command, "assign") == 0) {
+        if (strcmp(arg1, "userInput") == 0) sem_wait(&userInputMutex.semaphore);
+        if (strcmp(arg1, "userOutput") == 0) sem_wait(&userOutputMutex.semaphore);
+        if (strcmp(arg1, "file") == 0) sem_wait(&fileMutex.semaphore);
+        }
+    else if (strcmp(command, "semSignal") == 0) {
+        if (strcmp(arg1, "userInput") == 0) sem_signal(&userInputMutex.semaphore);
+        if (strcmp(arg1, "userOutput") == 0) sem_signal(&userOutputMutex.semaphore);
+        if (strcmp(arg1, "file") == 0) sem_signal(&fileMutex.semaphore);
+        }
+    else if (strcmp(command, "assign") == 0) {
         if (strcmp(arg1, "a") == 0) {
             if (strcmp(arg2, "input") == 0) {
                 assign("a", file_name);
                 memory[0] = atoi(file_name);
-            } else if (strcmp(arg2, "readFile") == 0) {
+                }
+            else if (strcmp(arg2, "readFile") == 0) {
                 read_file(file_name, buffer, sizeof(buffer));
                 memory[0] = atoi(buffer);
+                }
             }
-        }
         if (strcmp(arg1, "b") == 0) {
             if (strcmp(arg2, "input") == 0) {
                 assign("b", file_data);
                 memory[1] = atoi(file_data);
-            } else if (strcmp(arg2, "readFile") == 0) {
+                }
+            else if (strcmp(arg2, "readFile") == 0) {
                 read_file(file_name, buffer, sizeof(buffer));
                 memory[1] = atoi(buffer);
+                }
             }
         }
-    } else if (strcmp(command, "writeFile") == 0) {
+    else if (strcmp(command, "writeFile") == 0) {
         write_file(file_name, file_data);
-    } else if (strcmp(command, "readFile") == 0) {
+        }
+    else if (strcmp(command, "readFile") == 0) {
         read_file(file_name, buffer, sizeof(buffer));
-    } else if (strcmp(command, "printFromTo") == 0) {
+        }
+    else if (strcmp(command, "printFromTo") == 0) {
         print_from_to(memory[0], memory[1]);
-    } else if (strcmp(command, "print") == 0) {
+        }
+    else if (strcmp(command, "print") == 0) {
         print(buffer);
+        }
     }
-}
 
 void execute_program(Process* process) {
     FILE* file = fopen(process->program_file, "r");
@@ -148,7 +154,7 @@ void execute_program(Process* process) {
         printf("Error opening program file %s\n", process->program_file);
         process->state = DEAD; // Mark process as dead if file can't be opened
         return;
-    }
+        }
 
     char instruction[100];
     int time_spent = 0;
@@ -160,14 +166,14 @@ void execute_program(Process* process) {
         time_spent++;
         process->program_counter = ftell(file);
         print_memory();
-    }
+        }
 
     if (feof(file)) {
         process->state = DEAD;
-    }
+        }
 
     fclose(file);
-}
+    }
 
 
 
@@ -175,33 +181,34 @@ void enqueue(int pid) {
     if ((rear + 1) % QUEUE_SIZE == front) {
         printf("Ready queue is full\n");
         return;
-    }
+        }
     if (front == -1) front = 0;
     rear = (rear + 1) % QUEUE_SIZE;
     ready_queue[rear] = pid;
-}
+    }
 
 int dequeue() {
     if (front == -1) {
         printf("Ready queue is empty\n");
         return -1;
-    }
+        }
     int pid = ready_queue[front];
     if (front == rear) {
         front = rear = -1;
-    } else {
+        }
+    else {
         front = (front + 1) % QUEUE_SIZE;
-    }
+        }
     return pid;
-}
+    }
 
 void add_arriving_processes(Process* processes, int num_processes) {
     for (int i = 0; i < num_processes; i++) {
         if (processes[i].state == READY && processes[i].arrival_time == current_time) {
             enqueue(processes[i].pid);
+            }
         }
     }
-}
 
 void run_scheduler(Process* processes, int num_processes) {
     while (1) {
@@ -215,12 +222,12 @@ void run_scheduler(Process* processes, int num_processes) {
                 if (processes[i].state != DEAD) {
                     all_dead = 0;
                     break;
+                    }
                 }
-            }
             if (all_dead) break;
             current_time++;
             continue;
-        }
+            }
 
         Process* process = &processes[pid - 1];
 
@@ -232,19 +239,19 @@ void run_scheduler(Process* processes, int num_processes) {
             if (process->state != DEAD) {
                 process->state = READY;
                 enqueue(process->pid);
+                }
             }
-        }
 
         current_time++;
+        }
     }
-}
 
 int main() {
     Process processes[NUM_PROCESSES] = {
         { 1, READY, 0, "Program_1.txt", {0}, 0 },
         { 2, READY, 0, "Program_2.txt", {0}, 10 },
         { 3, READY, 0, "Program_3.txt", {0}, 20 }
-    };
+        };
 
     printf("Enter the time quantum: ");
     scanf("%d", &time_quantum);
@@ -252,9 +259,9 @@ int main() {
     for (int i = 0; i < NUM_PROCESSES; i++) {
         printf("Enter arrival time for process %d: ", i + 1);
         scanf("%d", &processes[i].arrival_time);
-    }
+        }
 
     run_scheduler(processes, NUM_PROCESSES);
 
     return 0;
-}
+    }
